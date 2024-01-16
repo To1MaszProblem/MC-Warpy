4a69bd0fa72cb66abb88aefdc75aaa2ad4843925
To1MaszProblem <tomecki695@gmail.com>
2024-01-16 19:32:41 +0100

    notice

--- src/main/java/pl/to1maszproblem/configuration/MessageConfiguration.java
+++ src/main/java/pl/to1maszproblem/configuration/MessageConfiguration.java
@@ -2,15 +2,14 @@ package pl.to1maszproblem.configuration;
 
 import eu.okaeri.configs.OkaeriConfig;
 import lombok.Getter;
+import pl.to1maszproblem.notice.Notice;
+import pl.to1maszproblem.notice.NoticeType;
 
 @Getter
 public class MessageConfiguration extends OkaeriConfig {
-    private String teleportCanceled = "&cTeleportacja została anulowana!";
-    private String teleportCanceledChatType = "CHAT";
-    private String timeToTeleport = "&fZostaniesz przeteleportowany za &a[time] &fsekund!";
-    private String timeChatType = "CHAT";
-    private String teleported = "&aZostałeś przeteleportowany do warpu [warp]!";
-    private String teleportedChatType = "SUBTITLE";
+    private Notice teleportCanceled = new Notice(NoticeType.MESSAGE,"&cTeleportacja została anulowana!");
+    private Notice timeToTeleport = new Notice(NoticeType.MESSAGE,"&fZostaniesz przeteleportowany za &a[time] &fsekund!");
+    private Notice teleported = new Notice(NoticeType.TITLE ,"&aZostałeś przeteleportowany do warpu [warp]!");
     private String holdItemToCreate = "&cMusisz trzymać item w ręce aby stworzyć warp!";
     private String warpAlreadyExist = "&cWarp o takiej nazwie już istnieje!";
     private String createdWarp = "&aPomyslnie stworzono warp o nazwie [warp-name]!";
